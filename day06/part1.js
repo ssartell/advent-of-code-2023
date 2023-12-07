d7e79bc0edba2cd8b38f354ecdd835a74e754c76
Sean Sartell <ssartell@rightpoint.com>
2023-12-06 22:39:55 -0600

    day06

--- day06/part1.js
+++ day06/part1.js
@@ -3,13 +3,11 @@ import * as R from 'ramda';
 const readLine = R.pipe(R.match(/\w+:\s+((?:\s*\d+)+)/), R.last, R.split(/\s+/), R.map(parseInt));
 const parseInput = R.pipe(R.split('\n'), R.map(readLine), R.apply(R.zip), R.map(([time, dist]) => ({time, dist})));
 
+const quadratic = (a, b, c) => [(-b+Math.sqrt(b*b-4*a*c))/(2*a), (-b-Math.sqrt(b*b-4*a*c))/(2*a)];
+
 const waysToBeat = race => {
-  let count = 0;
-  for(let i = 0; i <= race.time; i++) {
-    let dist = (race.time - i) * i;
-    count += dist > race.dist ? 1 : 0;
-  }
-  return count;
+  const [a, b] = quadratic(-1, race.time, -race.dist);
+  return Math.ceil(b) - Math.floor(a) - 1;
 }
 
 export default R.pipe(parseInput, R.map(waysToBeat), R.reduce(R.multiply, 1));
--- day06/part2.js
+++ day06/part2.js
@@ -3,13 +3,11 @@ import * as R from 'ramda';
 const readLine = R.pipe(R.match(/\w+:\s+((?:\s*\d+)+)/), R.last, R.replace(/\s+/g, ''), parseInt);
 const parseInput = R.pipe(R.split('\n'), R.map(readLine), ([time, dist]) => ({time, dist}));
 
+const quadratic = (a, b, c) => [(-b+Math.sqrt(b*b-4*a*c))/(2*a), (-b-Math.sqrt(b*b-4*a*c))/(2*a)];
+
 const waysToBeat = race => {
-  let count = 0;
-  for(let i = 0; i <= race.time; i++) {
-    let dist = (race.time - i) * i;
-    count += dist > race.dist ? 1 : 0;
-  }
-  return count;
+  const [a, b] = quadratic(-1, race.time, -race.dist);
+  return Math.ceil(b) - Math.floor(a) - 1;
 }
 
 export default R.pipe(parseInput, waysToBeat);
