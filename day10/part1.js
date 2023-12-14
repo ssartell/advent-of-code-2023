8525e225b32ba9882f15a79db72a5bf4bf6fbb73
Sean Sartell <ssartell@rightpoint.com>
2023-12-14 08:11:18 -0600

    day10

--- day10/part1.js
+++ day10/part1.js
@@ -1,10 +1,55 @@
 import * as R from 'ramda';
+import { bfs } from '../utils/graph-traversal.js';
+import { indexOf, getCardinalNeighbors, getValue } from '../utils/grid.js';
+import { toString, sub } from '../utils/vec2.js';
 
 const debug = x => { 
   debugger; 
   return x; 
 };
 
-const parseInput = R.pipe(R.split('\n'));
+const dirIndex = { '<0, -1>': 0, '<1, 0>': 1, '<0, 1>': 2, '<-1, 0>': 3 };
+const validityTable = {
+  // up, right, down, left
+  '|': [1,0,1,0],
+  '-': [0,1,0,1],
+  'L': [1,1,0,0],
+  'J': [1,0,0,1],
+  '7': [0,0,1,1],
+  'F': [0,1,1,0],
+  '.': [0,0,0,0],
+  'S': [1,1,1,1],
+};
 
-export default R.pipe(parseInput, debug);
+const isValidStep = (grid, a, b) => {
+  let aToB = validityTable[getValue(grid, a)][dirIndex[toString(sub(b, a))]];
+  let bToA = validityTable[getValue(grid, b)][dirIndex[toString(sub(a, b))]];
+  return aToB && bToA
+};
+
+const walkPipe = grid => {
+  let start = { ...indexOf(grid, 'S'), step: 0 };
+  let seen = new Set();
+  let last = null;
+  bfs(
+    start,
+    x => {
+      const key = toString(x);
+      if (seen.has(key)) {
+        debugger;
+      } else {
+        seen.add(key);
+        last = x;
+      }
+    },
+    x => getCardinalNeighbors(grid, x)
+      .filter(n => isValidStep(grid, x, n))
+      .map(n => ({ ...n, step: x.step + 1})),
+    toString
+  );
+  return last.step;
+};
+
+const parseInput = R.pipe(R.split('\n'), R.map(R.split('')));
+
+export default R.pipe(parseInput, walkPipe);
--- day10/part2.js
+++ day10/part2.js
@@ -1,10 +1,95 @@
 import * as R from 'ramda';
+import { bfs } from '../utils/graph-traversal.js';
+import { indexOf, getCardinalNeighbors, getValue, setValue } from '../utils/grid.js';
+import { toString, sub, add } from '../utils/vec2.js';
 
 const debug = x => { 
   debugger; 
   return x; 
 };
 
-const parseInput = R.pipe(R.split('\n'));
+const dirs = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}]
+const dirIndex = { '<0, -1>': 0, '<1, 0>': 1, '<0, 1>': 2, '<-1, 0>': 3 };
+const niceChars = {F: '╔', J: '╝', L: '╚', 7: '╗', '-': '═', '|': "║", '.': ' ', 'S': 'S' };
+const pipePatterns = {
+  // up, right, down, left
+  '|': [1,0,1,0],
+  '-': [0,1,0,1],
+  'L': [1,1,0,0],
+  'J': [1,0,0,1],
+  '7': [0,0,1,1],
+  'F': [0,1,1,0],
+  '.': [0,0,0,0],
+  'S': [1,1,1,1],
+};
+const findPipeMatch = pattern => {
+  for(let pipe of R.keys(pipePatterns)) {
+    if (R.all(x => x, R.zipWith(R.equals, pattern, pipePatterns[pipe]))) return pipe;
+  }
+};
 
-export default R.pipe(parseInput, debug);
+const isValidStep = (grid, a, b) => {
+  try {
+    let aToB = pipePatterns[getValue(grid, a)][dirIndex[toString(sub(b, a))]];
+    let bToA = pipePatterns[getValue(grid, b)][dirIndex[toString(sub(a, b))]];
+    return aToB && bToA
+  } catch (e) {
+    debugger;
+  }  
+};
+
+const walkPipe = grid => {
+  let start = { ...indexOf(grid, 'S'), step: 0 };
+  let loop = new Set([toString(start)]);
+  bfs(
+    start,
+    x => {
+      const key = toString(x);
+      if (!loop.has(key)) {
+        loop.add(key);
+      }
+    },
+    x => getCardinalNeighbors(grid, x)
+      .filter(n => isValidStep(grid, x, n))
+      .map(n => ({ ...n, step: x.step + 1})),
+    toString
+  );
+  return loop;
+};
+
+const countEnclosed = grid => {
+  let start = indexOf(grid, 'S');
+  let loop = walkPipe(grid);
+  let test = dirs
+    .map(x => add(start, x))
+    .map(x => loop.has(toString(x)) && isValidStep(grid, start, x) ? 1 : 0);
+  setValue(grid, start, findPipeMatch(test))
+  let enclosed = 0;
+  for(let y = 0; y < grid.length; y++) {
+    let intersections = 0;
+    let line = '';
+    for(let x = 0; x < grid[0].length; x++) {
+      let pos = {x,y};
+      let key = toString(pos);
+      let val = grid[y][x];
+      if (loop.has(key)) {
+        if (val === '|' || val === 'L' || val === 'J') {
+          intersections++;
+        }
+
+        line += niceChars[getValue(grid, pos)];
+      } else if (intersections % 2 === 1) {
+        enclosed++;
+        line += '@';
+      } else {
+        line += '.';
+      }
+    }
+    console.log(line);
+  }
+  return enclosed;
+};
+
+const parseInput = R.pipe(R.split('\n'), R.map(R.split('')));
+
+export default R.pipe(parseInput, countEnclosed);
