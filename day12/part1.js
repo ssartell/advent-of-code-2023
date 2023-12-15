87fd0b4bf2a3a9b559b258aade18a38639ff061c
Sean Sartell <ssartell@rightpoint.com>
2023-12-14 21:23:10 -0600

    day12

--- day12/part1.js
+++ day12/part1.js
@@ -13,7 +13,10 @@ const allCouldBeSprings = R.all(x => x === '#' || x === '?')
 
 const countArrangements = (row, groups, it) => {
   row = R.replace(/^\.+/, '', row);
-  if (groups.length === 0) return it;
+  if (groups.length === 0) {
+    console.log(it + (row.length > 0 ? R.repeat('.', row.length).join('') : ''));
+    return 1;
+  }
   if (row.length === 0) return 0;
   if (row.length < groups[0]) return 0;
 
@@ -21,11 +24,14 @@ const countArrangements = (row, groups, it) => {
   let [group, ...left] = groups;
   let after = row.substring(group, group + 1);
   if (allCouldBeSprings(row.substring(0, group)) && (after === '.' || after === '?' || after === '')) {
-    possibilities += countArrangements(row.slice(group + 1), left, it + R.repeat('#', group).join(''));
+    possibilities += countArrangements(row.slice(group + 1), left, it + R.repeat('#', group).join('') + '.');
   }
-  possibilities += countArrangements(R.replace(/^(?:\.|\?|\#+)/, '', row), groups, it + '.');
+  let size = R.match(/^(?:\.|\?)/, row)[0]?.length || 0;
+  if (size > 0) {
+    possibilities += countArrangements(R.replace(/^(?:\.|\?)/, '', row), groups, it + (size > 0 ? R.repeat('.', size).join('') : ''));
+  }
   
   return possibilities;
 };
 
-export default R.pipe(parseInput, R.map(({row, groups}) => countArrangements(row, groups, '')), debug);
+export default R.pipe(parseInput, R.map(({row, groups}) => countArrangements(row, groups, '')), debug, R.sum);
